e58cb11a5e152ddb2738f63ac04e14fb5c0b8cca
petermunachi <alimunachipeter@gmail.com>
2021-04-24 16:37:38 +0100

    adding fetch api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
+import React, { useState, useEffect, useRef  } from 'react';
+
 import './App.css';
+import RepoList from './components/RepoList'
 
 function App() {
+
+  // tracking on which page we currently are
+  const [page, setPage] = useState(1);
+  const [repos, setRepos] = useState([]);
+
+  // add loader refrence 
+  const loader = useRef(null);
+
+  useEffect(() => {
+    async function fetchRepos() {
+      try {
+        
+        const response = await fetch(`https://api.github.com/search/repositories?q=created:%3E2017-10-22&sort=stars&order=desc&page=${page}`);
+      
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const recentRepos = await response.json();
+        setRepos(recentRepos.items)
+
+      } catch(e) {
+        console.log(e);
+      }
+    }
+    fetchRepos();
+
+  },[]);
+
+
+  console.log(repos);
+  let repoItems = <div className="loader">Loading</div>
+
+  repoItems = repos.map(repo =>(
+    <RepoList 
+      key={repo.id} 
+      name={repo.name} 
+      issues={repo.open_issues_count} 
+      avatar={repo.owner.avatar_url} 
+      description={repo.description} 
+      stars={repo.stargazers_count}
+    />
+  ))
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {repoItems}
     </div>
   );
+
+
 }
 
 export default App;
---
+++ src/components/RepoList.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import '../App.css';
+
+function RepoList(props) {
+
+
+  return (
+    <div className="mt-3">
+
+      <div className="repo-list-container card d-flex ">
+  
+        <div className="avatar-container">
+          <img src={props.avatar} alt="" width="100" height="100" />
+        </div>
+        <div className="desc-container">
+          <h1 className="header-primary">{props.name}</h1>
+          <p className="description">{props.description}</p>
+  
+          <div className="d-flex ml-1 mt-3">
+            <div className="box">
+              Stars: <b>{props.stars}</b>
+            </div>
+            <div className="box pr-1">
+              Issues: <b>{props.issues}</b>
+            </div>
+            <div className="interval-container pr-1">
+              Submitted {props.interval} by <b>{props.name}</b>
+            </div>
+          </div>
+  
+        </div>
+        
+      </div>
+    
+    </div>
+  
+  );
+
+
+}
+
+export default RepoList;
